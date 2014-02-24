50215efd3058719ebb098b47f00b5c04af957313
Tohhy <olivinecafe@gmail.com>
2014-02-24 10:19:40 +0900

    some tests moved

---
+++ src/test/java/info/rosetto/parsers/ParserStateTest.java
@@ -0,0 +1,20 @@
+package info.rosetto.parsers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+
+public class ParserStateTest {
+
+    @Test
+    public void インスタンス化テスト() {
+        ParserState sut = new ParserState();
+        assertThat(sut.getTokens(), is(notNullValue()));
+        assertThat(sut.getParsingUnit(), is(nullValue()));
+        assertThat(sut.getUnitIndex(), is(0));
+    }
+}
